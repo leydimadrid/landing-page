f408f51168f01105a9764750aa5af3c34b891da9
Leydi <leydi.madrid6@gmail.com>
2022-09-05 08:25:27 -0500

    Se inicia con la página Habilidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Habilidades } from "./pages/components/Habilidades";
 import { Header } from "./pages/components/Header";
 import { Inicio } from "./pages/components/Inicio";
 import { SobreMi } from "./pages/components/SobreMi";
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Inicio />
       <SobreMi />
+      <Habilidades />
     </div>
   );
 }
---
+++ src/pages/components/Habilidades.jsx
@@ -0,0 +1,23 @@
+export const Habilidades = () => {
+  return (
+    <div className="habilidades">
+      <div className="text-2xl font-black text-center underline">
+        Habilidades
+      </div>
+      <div>Diseño gráfico</div>
+      <div className=" px-3 py-5 h-56 grid grid-cols-3 content-between">
+        <div>Animación 2D</div>
+        <div>Diseño digital</div>
+        <div>Diseño UI/UX</div>
+        </div>
+        <div>Desarrollo web</div>
+        <div className=" px-3 py-5 h-56 grid grid-cols-4 content-between">
+        <div>HTML</div>
+        <div>CSS</div>
+        <div>JavaScript</div>
+        <div>React</div>
+        </div>
+      
+    </div>
+  );
+};
